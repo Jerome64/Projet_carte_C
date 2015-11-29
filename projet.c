492587fb30f7ffa2927c5137d140716c9058b9d8
Jerome <jerome.briois@gmail.com>
2015-11-29 20:53:09 +0100

    Nouveau

--- projet.c
+++ projet.c
@@ -1,17 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define TAILLE 10
+
+void saisirTab(int iTab[], int * piNb){
+	
+	int i;
+	do{
+		printf("Combien d'éléments voulez vous dans votre tableau ?\n");
+		scanf("%d%*c",&iTab[*piNb]);
+		if(*piNb<TAILLE){
+			printf("Le tableau est limité à 10 nombres!");
+		}
+	}while(*piNb<TAILLE);
+	for(i=0;i<*piNb;i++){
+		printf("Saisissez un nombre:\n");
+		scanf("%d%*c",&iTab[i]);
+	}
+return;
+}
 
 int main(){
 
-<<<<<<< HEAD
-int a;
-=======
-	printf("test");
-	printf("un autre test");
->>>>>>> 64b44699e3fa46fd438be91ddea3080bfb645095
+int iTab[TAILLE];
+int iNb;
 
-	printf("Entrer une valeur:\n");
-	scanf("%d%*c",a);
 	
+	saisirTab(iTab,&iNb);
+
 	return 0;
 }
